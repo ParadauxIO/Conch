d3164232f598c45d6b1a7b6f83d271a5d5a23aa7
Rían Errity <rian@paradaux.io>
2020-10-05 16:40:27 +0100

    Invalidated testing webhook, oops!

--- src/test/java/io/paradaux/hiberniadiscord/WebhookUtils/GenericWebhookTest.java
+++ src/test/java/io/paradaux/hiberniadiscord/WebhookUtils/GenericWebhookTest.java
@@ -7,7 +7,7 @@ public class GenericWebhookTest {
 
     GenericWebhook webhook;
 
-    String webhookUrl = "https://discordapp.com/api/webhooks/735567483041349662/J9dviLWu51Dd723BPAiVC6WqB8oj6ke4NzWRzPx1v9hKnMf8lF84gjyapSsYBop_pj4m";
+    String webhookUrl = "";
     String webhookUserName = "HiberniaDiscord Unit Test";
     String webhookAvatarUrl = "https://cdn.paradaux.io/static/plugin-branding/hiberniadiscord/hibernia-discord.png";
     String webhookMessageContent = "This is a test";
